cce75d1d1ca56a100ba9339fa48b00856d6b4edc
Youssef & Rick <student@epicodus.com>
2015-10-12 21:01:26 -0700

    Add preliminary radio button functionality (broken)

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
   $("#intro form").submit(function(event){
     //alert("yeehaw");
 
+    var gayMar = parseInt($("input[name=radiogay]:radio").val());
+    var gun = parseInt($("input[name=radiogun]:radio").val());
+    var abo = parseInt($("input[name=radioabortion]:radio").val());
+
+    var score = gayMar + gun + abo
+
+    console.log(score);
+
     event.preventDefault();
   });
 
